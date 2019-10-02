72b1f6ff07cafbf47352b32aa3ada620118cd5d1
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-02 17:36:19 -0500

    not sure if valid_proof needs to be in both, but I am mining and it is working

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,12 +1,13 @@
 import hashlib
-import json
 import requests
+
+
 
 import sys
 
 
 # TODO: Implement functionality to search for a proof
-def proof_of_work(self, block):
+def proof_of_work(final_block):
     """
     Simple Proof of Work Algorithm
     Find a number p such that hash(last_block_string, p) contains 6 leading
@@ -14,13 +15,18 @@ def proof_of_work(self, block):
     :return: A valid proof for the provided block
     """
     # return proof
-    block_string = json.dumps(self.last_block, sort_keys=True).encode()
 
     proof = 0
-    while self.valid_proof(block_string, proof) is False:
+    while valid_proof(final_block, proof) is False:
         proof += 1
 
     return proof 
+
+def valid_proof(final_block, proof):
+        guess = f'{final_block}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -34,10 +40,24 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+
+        r = requests.get(url = node + '/last_block')
+        data = r.json()
+        final_block = data["final_block"]["previous_hash"]
+
+        new_proof = proof_of_work(final_block)
+
         # TODO: When found, POST it to the server {"proof": new_proof}
         # TODO: We're going to have to research how to do a POST in Python
         # HINT: Research `requests` and remember we're sending our data as JSON
+        proof_data = {"proof": new_proof}
+        r = requests.post(url = node + '/mine', json = proof_data)
+        data = r.json()
+        
         # TODO: If the server responds with 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get("message") == "New Block Forged":
+            coins_mined += 1
+            print(f"You have: {coins_mined} coins")
+        print(data.get("message"))
